c2af488c275ed5ad8b47ddd1f0445f4b684f6faa
adoka <adokanchi@gmail.com>
2025-01-15 03:02:06 -0800

    a

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date    |  Time  |                                                                                                                                                                                                                          Update |
+|:--------|:------:|--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| April 5 | 9-10pm |                                                                                                                                                                                      I tried to write abstract, efficient code. |
+| Jan 14  |        | Tried an approach where whenever I see a decrease in value, I delete the larger number. This is a solid-ish idea but fails in a lot of edge cases. Not fully sure what changes to make or if an approach like this is feasible. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * The class WeatherPatterns finds the longest span of days in which
  * each day’s temperature is higher than on the previous day in that sequence.
@@ -15,7 +17,50 @@ public class WeatherPatterns {
      * @return the longest run of days with increasing temperatures
      */
     public static int longestWarmingTrend(int[] temperatures) {
-        // TODO: Write your code here!
+        /*
+        ArrayList<Integer> temps = new ArrayList<Integer>();
+        for (int temp : temperatures) {
+            temps.add(temp);
+        }
+
+
+        int i = 1;
+        while (i < temps.size()) {
+            if (temps.get(i-1) >= temps.get(i)) {
+                temps.remove(i-1);
+            }
+            else {
+                i++;
+            }
+        }
+        return temps.size();
+
+         */
+
+        ArrayList<Integer> temps = new ArrayList<Integer>();
+        for (int temp : temperatures) {
+            temps.add(temp);
+        }
+
+
+        int last = temps.get(0);
+        int lowest = last;
+        int i = 1;
+        while (i < temps.size()) {
+            int curr = temps.get(i);
+            if (curr < lowest) {
+                // TODO
+                lowest = curr;
+            }
+            if (last >= curr) {
+                temps.remove(i-1);
+                i--;
+            }
+
+            i++;
+        }
+
+
 
         return 0;
     }
