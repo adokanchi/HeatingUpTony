d69ac29df4e1c5e93791879ed102b12dd04a850d
adoka <adokanchi@gmail.com>
2025-01-15 03:31:10 -0800

    b

--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -1,67 +1,37 @@
-import java.util.ArrayList;
-
 /**
  * The class WeatherPatterns finds the longest span of days in which
  * each day’s temperature is higher than on the previous day in that sequence.
  *
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Tony Dokanchi
  */
 
 public class WeatherPatterns {
-
-
     /**
      * Longest Warming Trend
-     * @param temperatures
+     * @param temperatures list of temperature values
      * @return the longest run of days with increasing temperatures
      */
     public static int longestWarmingTrend(int[] temperatures) {
-        /*
-        ArrayList<Integer> temps = new ArrayList<Integer>();
-        for (int temp : temperatures) {
-            temps.add(temp);
-        }
-
-
-        int i = 1;
-        while (i < temps.size()) {
-            if (temps.get(i-1) >= temps.get(i)) {
-                temps.remove(i-1);
+        int len = temperatures.length;
+        if (len == 1) return 1;
+        // seqLen[i] is the length of the longest valid sequence that ends at index i
+        // the solution to the problem is the largest value in seqLen
+        int[] seqLen = new int[len];
+        seqLen[0] = 1;
+        // longestSeq is the length of the longest sequence (aka the largest value in seqLen)
+        int longestSeq = 0;
+        for (int i = 1; i < len; i++) {
+            int longestSubseq = 0;
+            for (int j = 0; j < i; j++) {
+                if (temperatures[i] > temperatures[j] && seqLen[j] > longestSubseq) {
+                    longestSubseq = seqLen[j];
+                }
             }
-            else {
-                i++;
-            }
+            seqLen[i] = 1 + longestSubseq;
+            // Keep track of longestSeq while populating seqLen to avoid having to do a linear pass at the end
+            if (seqLen[i] > longestSeq) longestSeq = seqLen[i];
         }
-        return temps.size();
-
-         */
-
-        ArrayList<Integer> temps = new ArrayList<Integer>();
-        for (int temp : temperatures) {
-            temps.add(temp);
-        }
-
-
-        int last = temps.get(0);
-        int lowest = last;
-        int i = 1;
-        while (i < temps.size()) {
-            int curr = temps.get(i);
-            if (curr < lowest) {
-                // TODO
-                lowest = curr;
-            }
-            if (last >= curr) {
-                temps.remove(i-1);
-                i--;
-            }
-
-            i++;
-        }
-
-
-
-        return 0;
+        return longestSeq;
     }
 }
